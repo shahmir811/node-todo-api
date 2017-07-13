9089202cfd510844fd19fe1c1d0ea8e854ae50cf
shahmir811 <shahmirkj@gmail.com>
2017-07-13 11:32:22 +0500

    Updated mongoose.connect

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -7,9 +7,7 @@ let db = {
   mlab: 'mongodb://hello:hello123@ds157112.mlab.com:57112/todoapp'
 };
 
-//mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'); // Connect to Database
-
-mongoose.connect(process.env.PORT ? db.mlab : db.localhost); // Connect to Database
+mongoose.connect(process.env.PORT ? db.mlab : db.localhost, { useMongoClient: true }); // Connect to Database
 
 module.exports = {
   mongoose
