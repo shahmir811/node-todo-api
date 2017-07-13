3440b0693b648407b4fed08458c1bc063fea417c
shahmir811 <shahmirkj@gmail.com>
2017-07-13 17:12:57 +0500

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
@@ -91,6 +92,37 @@ app.delete('/todos/:id', (req, res) => {
 
 });
 
+// ***************************************************** //
+// Updating a todo
+// it uses patch method. PATCH method is used to update a record (recommended)
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']); // pick takes an oject and grabs only those properties that you want to change (like here we grab text and completed)
+
+  if (!ObjectID.isValid(id)) { // ObjectID.isValid(id) return true or false
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed) { //if completed is updated to true then updated the completedAt to UNIX timestamp
+    body.completedAt = new Date().getTime();
+  } else { //if completed is updated to false then updated the completedAt to null and completed to false
+    body.completed   = false;
+    body.completedAt = null;
+  }
+
+  //findByIdAndUpdate(filter, update, options, callback)
+  Todo.findByIdAndUpdate(id, { $set: body }, {new: true}).then( (todo) => { // { $set: body } means update the old body(text and completed). {new: true} will give us the updated value
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+
+  }).catch((err) => {
+    return res.status(400).send();
+  });
+
+});
 
 
 // ***************************************************** //
